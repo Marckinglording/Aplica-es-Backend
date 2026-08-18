1335ab90ff4800e94209cf5840799486b272b616
Marckinglording <dev.marcoslima@gmail.com>
2026-08-18 10:35:05 -0300

    Finalização da atividade passada

--- MostraMeses.java
+++ MostraMeses.java
@@ -4,8 +4,17 @@ public class MostraMeses {
         
         String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
 
-        System.out.println("================================");
-        
+        System.out.println("=================");
+        System.out.println("Mês- Nome do mês");
+        System.out.println("=================");
 
+        for(int i = 0; i < meses.length; i++) {
+
+            System.out.printf("%02d", (i + 1));
+            System.out.println("- " + meses[i]);
+        }
+
+        System.out.println("=================");
+        
     }
 }
